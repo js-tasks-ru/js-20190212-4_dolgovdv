a963c4b7b706baef515db460dc78fa80b3aa004b
dolgovdv <dolgov.denis@gmail.com>
2019-02-19 16:57:54 +0300

    возврат тестового решения на проверку
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -5,4 +5,8 @@
  * @returns {number}
  */
 function sum (m, n) {
+  if ( isNaN(parseFloat(m)) || isNaN(parseFloat(n)) || !isFinite(m) || !isFinite(n) ){
+    return ('Error: not numeric.')
+  }
+  return (m + n)
 }
