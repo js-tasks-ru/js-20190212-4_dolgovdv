383e0c2da01d1d5b06bb43e0b963973c14a1c864
dolgovdv <dolgov.denis@gmail.com>
2019-02-20 23:30:41 +0300

    function pow(m, n)
    Signed-off-by: dolgovdv <dolgov.denis@gmail.com>

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -10,4 +10,4 @@ function sum(m, n) {
   }
   return (m + n);
 }
-sum(1, 2);
+sum(3, 2);
--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -8,7 +8,16 @@ function pow(m, n) {
   if (Number.isNaN(parseFloat(m)) || Number.isNaN(parseFloat(n)) || !Number.isFinite(m) || !Number.isFinite(n)) {
     return ('Error: not numeric.');
   }
-  const a = '';
-  return (a);
+  if (n < 0) {
+    return ('Error: not numeric.');
+  }
+  let a = 1;
+  if (n === 0) {
+    return a;
+  }
+  for (let i = 0; i < n; i += 1) {
+    a *= m;
+  }
+  return a;
 }
 pow(2, 3);
